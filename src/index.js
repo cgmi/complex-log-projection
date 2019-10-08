8a6ed7a78b3fdc917e1abdfa9df0ea68ecb52fc4
Matthias Albrecht <matthias.albrecht@uni-konstanz.de>
2019-10-08 22:50:18 +0200

    SVG scaling

--- src/index.js
+++ src/index.js
@@ -96,7 +96,7 @@ function renderSvg() {
     const width =  svg.attr("width");
     const [[x0, y0], [x1, y1]] = d3.geoPath(projection.fitWidth(width, world.outline)).bounds(world.outline);
     const dy = Math.ceil(y1 - y0), l = Math.min(Math.ceil(x1 - x0), dy);
-    projection.scale(projection.scale() * (l - 1) / l).precision(0.2);
+    projection.scale(renderParams.scaleFactor * projection.scale() * (l - 1) / l).precision(0.2);
     const height = dy;
 
     // Sync SVG settings
